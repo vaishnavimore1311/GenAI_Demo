a44900107eed0cb78e4ae531c563d3d4ad423e40
Vaishnavi More <vaishnavomore1113@gmail.com>
2026-09-19 15:26:06 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm=ChatGroq(
+    model="openai/gpt-oss-20b"
+)
+
+prompt = input("Enter your prompt: ")
+response = llm.invoke(prompt)
+print(response.content)
